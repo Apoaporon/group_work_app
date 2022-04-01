045fdf942ea7413dfac3b8cd3cf3cc470afcc47c
Tatsuya Kawakami <usagon620@gmail.com>
2022-04-01 23:07:18 +0900

    create group work app

---
+++ frontend/src/router/index.js
@@ -0,0 +1,33 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+const routerOptions = [
+  { path: '/', component: 'Home' },
+  { path: '/about', component: 'About' }
+]
+
+const routes = routerOptions.map(route => {
+  return {
+    ...route,
+    component: () => import(`@/components/${route.component}.vue`)
+  }
+})
+
+Vue.use(Router)
+
+export default new Router({
+  routes,
+  mode: 'history'
+})
+// import HelloWorld from '@/components/HelloWorld'
+//
+// Vue.use(Router)
+//
+// export default new Router({
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'HelloWorld',
+//       component: HelloWorld
+//     }
+//   ]
+// })
